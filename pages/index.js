03c095511915b0b2e793a6f52ceee688765ee1dd
JoanCollaguazo <joangamers12@gmail.com>
2021-09-15 15:45:38 -0500

    P2

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,6 @@ const Index = () =>{
         <Navigation/>
         <h1>Index</h1>
         <h1>HOLA MUNDO ESTOY EN PRUEBA</h1>
-        <h3>Joan tonto de mierda</h3>
-        <h2> jsdnakjfnkjadsnjk</h2>
         </div>
     )
 }
