fbc64aff8d693334f0a20c8490bcf7b0b8142183
JoanCollaguazo <joangamers12@gmail.com>
2021-09-17 16:11:16 -0500

    C1

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navigation from "../components/navigation";
+
+const inicio=()=>{
+    return(
+    <div>
+        <Navigation/>
+        <h1>Inicio</h1>
+    </div>
+    )
+    }
+export default inicio;    
