5debde6404c297a9d3303e5328687820ea7be026
OxOrboy <joangamers12@gmail.com>
2021-09-20 16:23:21 -0500

    Adapatacion de titulos

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,14 @@
 import React from "react";
 import Navigation from "../components/navigation";
 
-
-const index=()=>{
-    return(
+const index = () => {
+  return (
     <div>
-        <Navigation/>
+      <Navigation title="FACTURO">
         <h1>Inicio</h1>
+      </Navigation>
     </div>
-    )
-    }
+  );
+};
 
-export default index;    
-
-
+export default index;
--- pages/registro.js
+++ pages/registro.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Navigation from "../components/navigation";
 
-const registro=()=>{
-    return(
+const registro = () => {
+  return (
     <div>
-        <Navigation/>
-        <h1><br></br>Registro</h1>
+      <Navigation title="REGISTRO - FACTURO">
+        <h1>Registro</h1>
+      </Navigation>
     </div>
-    )
-    }
-export default registro;    
+  );
+};
+export default registro;
